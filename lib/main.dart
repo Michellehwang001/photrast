dce3721296bf5c586e825f0fbecda30ce084da95
Hwang Hyejung <jimin_ma@naver.com>
2021-06-28 11:56:29 +0900

    start Photorast

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,46 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Photrast(),
+    );
+  }
+}
+
+class Photrast extends StatelessWidget {
+  const Photrast({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            // Background Image
+
+                Expanded(
+                    child: Container(
+                        child: Image.asset(
+                  'assets/first_bg.jpeg',
+                  fit: BoxFit.fill,
+                ))),
+                // Container(
+                //   height: 300,
+                //   width: 300,
+                //   padding: const EdgeInsets.all(8.0),
+                //   margin: const EdgeInsets.all(20.0),
+                //   decoration: BoxDecoration(
+                //     border: Border.all(width: 1),
+                //     color: Colors.transparent,
+                //   ),
+                //   // child: Material( colo
+                //   // ),
+                // ),
+
+          ],
+        ),
+      ),
     );
   }
 }
